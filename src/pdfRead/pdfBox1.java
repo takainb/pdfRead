81a65be107056167a827c747c339ed6de6406f5a
DESKTOP-EHR3GF3\takai <takai@fintechsys.co.jp>
2018-06-19 10:36:53 +0900

    init

--- src/pdfRead/pdfBox1.java
+++ src/pdfRead/pdfBox1.java
@@ -3,10 +3,12 @@ package pdfRead;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -22,41 +24,39 @@ public class pdfBox1 {
 	
 	public static final String INPUT_PATH = "C:\\test\\pdf\\";
 	public static final String OUTPUT_PATH = "C:\\test\\result\\";
-	public static final String DEFINE_FILE = "C:\\test\\item\\item.txt";
+	public static final String DEFINE_ITEM_FILE = "C:\\test\\item\\item.txt";
+	public static final String DEFINE_HEAD_FILE = "C:\\test\\item\\header.txt";
 	public static final String PDF = ".pdf";
 	public static final String CSV = ".csv";
 	public static final String AST = "(*)";
 	
 	public static void main(String...args) throws IOException {
 		
-		// 引数Check
 		if (args.length != 1) {
-			System.out.println("引数エラーです");
+			System.out.println("引数エラー");
 		} else {
 			String filename = args[0].replace(PDF, "");
 			String inputFile = INPUT_PATH + args[0];
 			String outputFile = OUTPUT_PATH + filename + CSV;
-			
+
+			LinkedHashMap<Integer, String> headerMap = new LinkedHashMap<Integer, String>();
 			LinkedHashMap<Integer, LinkedHashMap<String, String>> itemListMap = new LinkedHashMap<Integer, LinkedHashMap<String, String>>();
 			List<String> pdfData = new ArrayList<String>();
-			
-			// 項目定義の読み込み
-			readItem(DEFINE_FILE, itemListMap);
-			// PDF読み取り
+
+			readHeader(DEFINE_HEAD_FILE, headerMap);
+			readItem(DEFINE_ITEM_FILE, itemListMap);
 			readPDFFile(inputFile, pdfData);
-			// 対象データ抜き取り
-			List<Map<String, String>> dataMap = targetDataExtra(itemListMap, pdfData);
 			
-			// データ加工
+//			List<Map<String, String>> dataMap = targetDataExtra(itemListMap, pdfData);
+			List<Map<String, String>> dataMap = targetDataExtra(headerMap, itemListMap, pdfData);
+			
 			Map<String, String[]> dataList = dataProcessing(dataMap);
 			
-			// 結果出力
 			outputResult(dataList, outputFile);
 		}
 	}
 
 	private static void outputResult(Map<String, String[]> dataList, String outputFile) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
 		FileWriter fw = new FileWriter(outputFile,false);
 		PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
 		
@@ -76,12 +76,13 @@ public class pdfBox1 {
 	}
 
 	private static Map<String, String[]> dataProcessing(List<Map<String, String>> dataMap) {
-		// TODO 自動生成されたメソッド・スタブ
 		Map<String, String[]> map = new LinkedHashMap<String, String[]>();
 		
 		for (Iterator<Map<String, String>> it = dataMap.iterator(); it.hasNext();) {
 			for (Iterator<Entry<String, String>> itt = it.next().entrySet().iterator();itt.hasNext();) {
 				Entry<String, String> entry = itt.next();
+
+				// DEBUG
 				System.out.println(entry.getKey() + " : " + entry.getValue());
 				
 				String[] str = dataTrim(entry.getKey(), entry.getValue());
@@ -92,7 +93,6 @@ public class pdfBox1 {
 	}
 
 	private static String[] dataTrim(String key, String value) {
-		// TODO 自動生成されたメソッド・スタブ
 		String line = null;
 		String[] data = null;
 		String str = key + AST;
@@ -101,81 +101,154 @@ public class pdfBox1 {
 		line = line.replace(key, "");
 		
 		line = line.trim();
-		data = line.split(" ");
+		data = line.split("[\\s]+");
+//		data = line.split(" ");
 		
 		return data;
 	}
-
-	private static List<Map<String, String>> targetDataExtra(LinkedHashMap<Integer, LinkedHashMap<String, String>> itemListMap,
-			List<String> pdfData) {
-		// TODO 自動生成されたメソッド・スタブ
+	
+	private static List<Map<String, String>> targetDataExtra(LinkedHashMap<Integer, String> headerMap,
+			LinkedHashMap<Integer, LinkedHashMap<String, String>> itemListMap, List<String> pdfData) {
+		
 		List<Map<String, String>> data = new ArrayList<Map<String, String>>();
 		Map<String, String> map = new LinkedHashMap<String, String>();
-		for (Iterator<String> it1 = pdfData.iterator(); it1.hasNext();) {
-			String strPdfdata = it1.next();
-			for (Iterator<Entry<Integer, LinkedHashMap<String, String>>> it2 = itemListMap.entrySet().iterator(); it2
-					.hasNext();) {
-				Entry<Integer, LinkedHashMap<String, String>> entryItemMap = it2.next();
+		String strData = null;
+		// Header
+		for (Iterator<Entry<Integer, String>> it_head = headerMap.entrySet().iterator(); it_head.hasNext();) {
+			Entry<Integer, String> entryHeaderMap = it_head.next();
+			
+			boolean isSearch = false;
+			
+			// Header定義がPDFデータに存在しているか
+			for (Iterator<String> it_pdf = pdfData.iterator(); it_pdf.hasNext();) {
+				String strPdfdata = it_pdf.next();
 				
-				for(Iterator<Entry<String, String>> it3 = entryItemMap.getValue().entrySet().iterator(); it3.hasNext();) {
-					Entry<String, String> entryItemData = it3.next();
-					
-					if(strPdfdata.startsWith(entryItemData.getKey())) {
-						if ("S".equals(entryItemData.getValue())) {
-							int a = entryItemMap.getKey();
-						} else {
-							map.put(entryItemData.getKey(), strPdfdata);
-							it3.remove();
-							
-							if("E".equals(entryItemData.getValue())) {
-								data.add(map);
-								return data;
-							}
+				if (!isSearch) {
+					if (strPdfdata.startsWith(entryHeaderMap.getValue())) {
+						// 存在した場合、次の行以降を検索
+						isSearch = true;
+					}
+					continue;
+				}
+
+				// 存在した場合、項目定義データを取り出す
+				Map<String, String> itemMap = itemListMap.get(entryHeaderMap.getKey());
+				for (Iterator<Entry<String, String>> it_item = itemMap.entrySet().iterator();it_item.hasNext();) {
+					Entry<String, String> entryItemData = it_item.next();
+					if (strPdfdata.startsWith(entryItemData.getKey().split("\r\n")[0])) {
+						if (!"1".equals(entryItemData.getValue().split("-")[1])) {
+							strData = strData.concat(strPdfdata.replace("\r\n", ""));
+							continue;
+						}
+						map.put(entryItemData.getKey(), strPdfdata);
+						it_item.remove();
+
+						if ("E".equals(entryItemData.getValue().split("-")[0])) {
+							data.add(map);
+							return data;
 						}
 					}
 				}
+				
 			}
+			
 		}
-		return data;
+		
+		return null;
 	}
+	
+//	private static List<Map<String, String>> targetDataExtra(LinkedHashMap<Integer, LinkedHashMap<String, String>> itemListMap,
+//			List<String> pdfData) {
+//		List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+//		Map<String, String> map = new LinkedHashMap<String, String>();
+//		String strData = null;
+//		// PDF Data
+//		for (Iterator<String> it1 = pdfData.iterator(); it1.hasNext();) {
+//			String strPdfdata = it1.next();
+//			
+//			for (Iterator<Entry<Integer, LinkedHashMap<String, String>>> it2 = itemListMap.entrySet().iterator(); it2
+//					.hasNext();) {
+//				Entry<Integer, LinkedHashMap<String, String>> entryItemMap = it2.next();
+//				
+//				for(Iterator<Entry<String, String>> it3 = entryItemMap.getValue().entrySet().iterator(); it3.hasNext();) {
+//					Entry<String, String> entryItemData = it3.next();
+//					
+//					if (strPdfdata.startsWith(entryItemData.getKey().split("\r\n")[0])) {
+//						if (!"1".equals(entryItemData.getValue().split("-")[1])) {
+//							strData = strData.concat(strPdfdata.replace("\r\n", ""));
+//							continue;
+//						}
+//						map.put(entryItemData.getKey(), strPdfdata);
+//						it3.remove();
+//
+//						if ("E".equals(entryItemData.getValue())) {
+//							data.add(map);
+//							return data;
+//						}
+//					}
+//				}
+//			}
+//		}
+//		return data;
+//	}
 
 	private static void readPDFFile(String inputFile, List<String> pdfData) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
 		File file = new File(inputFile);
 		try (PDDocument document = PDDocument.load(file)){
 			PDFTextStripper s = new PDFTextStripper();
 			String text = s.getText(document);
-//			System.out.println("[" + text + "]");
+			
+			// DEBUG
+			System.out.println("[" + text + "]");
 			
 			String[] str = text.split("\r\n");
 			for (int i=0; i < str.length; i++) {
 				pdfData.add(str[i]);
 			}
-			
 			document.close();
 		}
 	}
 
 	private static void readItem(String defineFile, LinkedHashMap<Integer, LinkedHashMap<String, String>> map2)
 			throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
 		File file = new File(defineFile);
 		String line = null;
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		
-		while((line = br.readLine()) != null) {
+		// BufferedReader br = new BufferedReader(new FileReader(file));
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+
+		while ((line = br.readLine()) != null) {
 			String[] str = line.split(",");
-			
+
+			// str[0]: Type
+			// str[1]: Start/End - Repeat
+			// str[2]: Item Name
 			int a = Integer.parseInt(str[0]);
 			map.put(str[2], str[1]);
-			
-			if("E".equals(str[1])) {
+
+			if ("E".equals(str[1].split("-")[0])) {
 				map2.put(a, map);
-				map = null;
+				map = new LinkedHashMap<String, String>();
 			}
 		}
 		br.close();
 	}
+	
+	private static void readHeader(String defineHeadFile, LinkedHashMap<Integer, String> headerMap) throws IOException {
+		File file = new File(defineHeadFile);
+		String line = null;
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
 
+		while ((line = br.readLine()) != null) {
+			String[] str = line.split(",");
+
+			// str[0]: Type
+			// str[1]: Header Name
+			int a = Integer.parseInt(str[0]);
+			headerMap.put(a, str[1]);
+		}
+		br.close();
+	}
 }
